8cf7a746ba4176b7b4a849d794725fbb53d5454f
Andrew Anderson <aanderso@tcd.ie>
2019-04-05 16:25:01 +0100

    Add names for quantized blobs

--- triNNity/triNNity/frontend/transformers.py
+++ triNNity/triNNity/frontend/transformers.py
@@ -275,8 +275,18 @@ class ParameterNamer(object):
                 continue
             if node.kind in (LayerKind.Convolution, LayerKind.InnerProduct):
                 names = ('weights',)
+                if node.parameters.mask_term:
+                    names += ('masked_weights',)
+                if node.parameters.quantize_term:
+                    names += ('quantized_weights',)
+
                 if node.parameters.bias_term:
                     names += ('biases',)
+                    if node.parameters.mask_term:
+                      names += ('masked_biases',)
+                    if node.parameters.quantize_term:
+                      names += ('quantized_biases',)
+
             elif node.kind == LayerKind.BatchNorm:
                 names = ('mean', 'variance')
                 if len(node.data) == 4:
