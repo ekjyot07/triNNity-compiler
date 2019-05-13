daa3e5a860d28959742f8dad131b3501cd3111ab
ekjyot07 <esingh@tcd.ie>
2019-05-13 17:51:18 +0100

    testing

--- triNNity/triNNity/backend/armcl/transformer.py
+++ triNNity/triNNity/backend/armcl/transformer.py
@@ -48,8 +48,8 @@ class ARMCLNode(object):
             print(temp)
             # args = ', '.join([str(int(args[3]))+'U', str(int(args[3]))+'U', str(int(args[6]))+'U', 'get_weights_accessor(data_path, "/cnn_data/' + graphName.lower() + '_model/' + self.node.name.lower() + '_w.npy", weights_layout)'] + [
             #                  'get_weights_accessor(data_path, "/cnn_data/'+ graphName.lower() +'_model/'+(self.node.name())+'_b.npy"), PadStrideInfo(' + str(int(args[5])), str(int(args[6])), str(int(args[9])), str(int(args[9])) + ')'])
-            if (self.kwargs['group'] != 1):
-                args.append(',' + self.kwargs['group'] + ')') 
+            # if (self.kwargs['group'] != 1):
+            #     args.append(',' + self.kwargs['group'] + ')') 
 
         elif (self.op == 'relu'):
             self.op = 'ActivationLayer'
